5c07e002ea3432597c1c469bcad23c45c77ea827
Beki <bereketadego@gmail.com>
2022-04-15 08:29:12 +0300

    my_list issue fix

--- my_list.rb
+++ my_list.rb
@@ -6,19 +6,7 @@ class MyList
     @list = list
   end
 
-  def each
-    i = @list.length
-    while i >= 0
-      puts @list[i]
-      i -= 1
-    end
+  def each(&block)
+    @list.each(&block)
   end
 end
-
-list = MyList.new(1, 2, 3, 4)
-list.each
-list.all? { |e| e < 5 }
-list.all? { |e| e > 5 }
-list.any? { |e| e == 2 }
-list.any? { |e| e == 5 }
-list.filter(&:even?)
